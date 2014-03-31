47913da6ffd27896c3b90b4f923bc75f733b70e8
Riley Chapman <rlychpmn@gmail.com>
2014-03-31 12:53:47 -0400

    working on chromosome driver

--- driveMatrixEvolution.py
+++ driveMatrixEvolution.py
@@ -1,3 +1,32 @@
+"""
+Facilitates Genetic Matrix evolution
+
+@author: Riley Chapman
+
+@teamName: Track Bot Driver (TBD)
+"""
+
+import matrixEvolution
+
+def createChromosome(l):
+	""" Builds a chromosome out of matrices
+		l: length of chromosome
+		returns: list of matrices representing a chromosome
+	"""
+	chromosome = []
+	for i in range(l):
+		chromosome.append(matrixEvolution.formMatrix(2,2))
+	return chromosome
+	
+def evolve(fittest):
+	""" Takes in the best genes and creates a new chromosome for the next generation
+		fittest: the best genes from the previous generation
+	"""
+	mixMatrix
+	mutate
 
 
-def driveMatrix():
+
+if __name__ == '__main__':
+	chrom = createChromosome(2)
+	print chrom
