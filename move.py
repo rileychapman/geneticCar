d29f3fe3730dbe8e8c1cff72aa27c7b1c5b91927
Josh Sapers <josh@josh-Latitude-E6430.(none)>
2014-04-03 14:36:20 -0400

    adding moving

--- move.py
+++ move.py
@@ -6,28 +6,29 @@ Created on Thu Apr  3 13:48:15 2014
 """
 from math import *
 class motion:
-    def __init__(self,x,y,t1,t2,m1,m2,mt,l,theta):
+    def __init__(self,x,y,w1,w2,m1,m2,mt,l,theta):
         self.x = x        
         self.y = y
         self.vx = 0        
         self.vy = 0
         self.theta=theta
-        self.t1=t1
-        self.t2=t2
+        self.w1=w1
+        self.w2=w2
         self.m1=m1
         self.m2=m2
         self.mt=mt
         self.l=l
         self.w=0
     def turn(self):
-        t=self.t1-self.t2
+        t=self.w1-self.w2
         I = l * (self.m1+self.m2)*(self.m1+self.m2)/12.0
         self.w = t/I
     def move(self):
-        self.theta+=self.w
-        self.vx+=(self.t1+self.t2)/self.mt*cos(theta)
-        self.vy+=(self.t1+self.t2)/self.mt*sin(theta)
-        self.x+=self.vx
+        self.theta+=self.w #updates angle
+        f = self.w1+self.w2 #force on the car
+        self.vx+=f/self.mt*cos(theta) #updates velocity
+        self.vy+=f/self.mt*sin(theta) 
+        self.x+=self.vx#updates position
         self.y+=self.vy
         
         
