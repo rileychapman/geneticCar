64cea6c5d273effe0ca3927c9a99a293eff243f1
Paul Titchener <paul.titchener@students.olin.edu>
2014-04-05 13:38:28 -0400

    Track drawing and offsetting code

--- modelview.py
+++ modelview.py
@@ -5,14 +5,16 @@
 
 import pygame, random, math, time
 from pygame.locals import *
+import math
 
 class Wall(object):
     def __init__(self, pos):
         walls.append(self)
-        self.rect = pygame.Rect(pos[0], pos[1], 20, 20)
-
+        self.rect = pygame.Rect(pos[0], pos[1], 10, 10)
+        self.pos = pos
+"""
 def hold_levels():
-    """Normal function that holds our levels as lists. Any other way is too hard"""
+    Normal function that holds our levels as lists. Any other way is too hard
     level = [[
         "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
         "W                                 W",
@@ -59,21 +61,26 @@ def change_to_list(num):
     return walls
     
 blocks = change_to_list(0)
-        
+"""        
 class Platformer_Model:
     """ Encodes the game state """
     """TO-DO: Clean up these level lists"""
     def __init__(self):
-        self.level1 = change_to_list(0)
-        self.duck = Duck()
+        #self.level1 = change_to_list(0)
+        self.duck = Duck(self,(100,100))
+        self.drawTrack = False
+        self.drawMode = True
+        self.Track = []
+        self.FinalTrack = [[],[]]
         
     def update(self):
         self.duck.update(vx, vy)
 
 class Duck:
     """Code for moving car"""
-    def __init__(self):
-        self.rect = pygame.Rect(32, 32, 16, 16)
+    def __init__(self,model,pos):
+        self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
+        self.model = model
 #        self.x = 0
 #        self.y = 0
 #        self.vy = 0.0
@@ -93,7 +100,17 @@ class Duck:
         self.rect.y += vy
     
         # If you collide with a wall, move out based on velocity
-        for wall in blocks:
+        for wall in self.model.FinalTrack[0]:
+            if self.rect.colliderect(wall.rect):
+                if vx > 0: # Moving right; Hit the left side of the wall
+                    self.rect.right = wall.rect.left
+                if vx < 0: # Moving left; Hit the right side of the wall
+                    self.rect.left = wall.rect.right
+                if vy > 0: # Moving down; Hit the top side of the wall
+                    self.rect.bottom = wall.rect.top
+                if vy < 0: # Moving up; Hit the bottom side of the wall
+                    self.rect.top = wall.rect.bottom
+        for wall in self.model.FinalTrack[1]:
             if self.rect.colliderect(wall.rect):
                 if vx > 0: # Moving right; Hit the left side of the wall
                     self.rect.right = wall.rect.left
@@ -129,23 +146,87 @@ class PyGameWindowView:
             pygame.draw.rect(screen, pygame.Color(255, 255, 255), wall.rect)          
         pygame.display.update()
 
+    def draw1(self):
+        self.screen.fill(pygame.Color(0,0,0))
+        pygame.draw.rect(screen, pygame.Color(0,255,0), model.duck.rect)
+        if self.model.drawMode == True:
+            for trackblock in model.Track:
+                pygame.draw.rect(screen,pygame.Color(255,255,255),trackblock.rect)
+        else:
+            for trackblock in model.FinalTrack[0]:
+                pygame.draw.rect(screen,pygame.Color(255,255,255),trackblock.rect)
+            for trackblock in model.FinalTrack[1]:
+                pygame.draw.rect(screen,pygame.Color(255,255,255),trackblock.rect)
+        pygame.display.update()
 
-class PyGameKeyboardController:
+
+
+class PyGameController:
     """ Manipulate game state based on keyboard input """
     def __init__(self, model):
         self.model = model
     
     def handle_pygame_event(self, event):
-        if event.type != KEYDOWN:
+        if event.type == KEYDOWN:
+
+            if event.key == pygame.K_LEFT:
+                self.model.duck.update(-10, 0)
+            if event.key == pygame.K_RIGHT:
+                self.model.duck.update(10,0)
+            if event.key == pygame.K_UP:
+                self.model.duck.update(0,-10)
+            if event.key == pygame.K_DOWN:
+                self.model.duck.update(0,10)
+        if event.type == MOUSEBUTTONDOWN:
+            self.model.drawTrack = True
+    
+        elif event.type == MOUSEBUTTONUP:
+            self.model.drawTrack=False
+            self.model.drawMode =False
+        else:
             return
-        if event.key == pygame.K_LEFT:
-            self.model.duck.update(-10, 0)
-        if event.key == pygame.K_RIGHT:
-            self.model.duck.update(10,0)
-        if event.key == pygame.K_UP:
-            self.model.duck.update(0,-10)
-        if event.key == pygame.K_DOWN:
-            self.model.duck.update(0,10)
+
+    def draw_track(self):
+        wallBlock = Wall(pygame.mouse.get_pos())
+        model.Track.append(wallBlock)
+
+
+    def offset_track(self,radius):
+        i = 0
+        for element in self.model.Track:
+            xDiff = float((element.pos[0] - self.model.Track[i-1].pos[0]))
+            yDiff = float((element.pos[1] - self.model.Track[i-1].pos[1]))
+
+            if xDiff == 0 and yDiff ==0:
+                pass
+            elif xDiff != 0:
+                xSign = xDiff/abs(xDiff)
+                slope = yDiff/xDiff#float((element.pos[1] - self.model.Track[i-1].pos[1]))/(element.pos[0] - self.model.Track[i-1].pos[0])
+                if slope != 0:
+                    ySign = yDiff/abs(yDiff)
+                    slopeSign = slope/abs(slope)
+
+                    perpendicularSlope = -1.0/slope
+                    angle = math.atan(perpendicularSlope)
+                    innerPos = (element.pos[0] + abs(math.cos(angle))*radius*(xSign**2*ySign) , element.pos[1] + abs(math.sin(angle))*radius*-(xSign*ySign**2))
+                    outerPos = (element.pos[0] - abs(math.cos(angle))*radius*(xSign**2*ySign) , element.pos[1] - abs(math.sin(angle))*radius*-(xSign*ySign**2))
+                else:
+                    innerPos = (element.pos[0] , element.pos[1] + radius*-xDiff/abs(xDiff))
+                    outerPos = (element.pos[0] , element.pos[1] - radius*-xDiff/abs(xDiff))
+
+            else:
+                innerPos = (element.pos[0] +radius*yDiff/abs(yDiff), element.pos[1])
+                outerPos = (element.pos[0] -radius*yDiff/abs(yDiff), element.pos[1])
+
+            innerBlock = Wall(innerPos)
+            outerBlock = Wall(outerPos)
+            self.model.FinalTrack[0].append(innerBlock)
+            self.model.FinalTrack[1].append(outerBlock)
+
+
+
+            i +=1
+
 
 
 if __name__ == '__main__':
@@ -155,7 +236,7 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size)
     model = Platformer_Model()
     view = PyGameWindowView(model,screen)
-    controller = PyGameKeyboardController(model)
+    controller = PyGameController(model)
 #    car = Duck()
 #    pygame.draw.rect(screen, (255, 200, 0), car.rect)
 
@@ -166,11 +247,16 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
-            if event.type == KEYDOWN:
+            if event.type == KEYDOWN or event.type == MOUSEBUTTONDOWN or event.type == MOUSEBUTTONUP:
                 controller.handle_pygame_event(event)
+            if model.drawTrack == True and model.drawMode == True:
+                controller.draw_track()
+            if model.drawTrack == False and model.drawMode ==False:
+                controller.offset_track(50)
+
         
 #        model.update()
-        view.draw()
+        view.draw1()
         time.sleep(0.001)
 
     pygame.quit()
