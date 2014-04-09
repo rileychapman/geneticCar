b53aa26d3219bc19e91840d5acf9fe932589d937
Riley Chapman <rlychpmn@gmail.com>
2014-04-09 15:51:30 -0400

    updating code that runs pygame

--- geneticCar.py
+++ geneticCar.py
@@ -6,53 +6,55 @@ Genetic Car Algorithm
 @teamName: Track Bot Driver (TBD)
 """
 import driveMatrixEvolution
-import modelViewController
+import Model
+import View
+import Controller
+
 import pygame, random, math, time
 from pygame.locals import *
 import math
 
-if __name__ == '__main__':
-	#drawTrack
-	pygame.init()
-	walls = []
-	size = (1200, 900)
 
-	screen = pygame.display.set_mode(size)
-	model = modelViewController.Platformer_Model(size)
-	view = modelViewController.PyGameWindowView(model,screen)
-	controller = modelViewController.PyGameController(model)
+pygame.init()
+walls = []
+size = (500, 500)
 
-	running = True
-
-	while running:
-	       
-	    for event in pygame.event.get():
-	        if event.type == QUIT:
-	            running = False
-	        if event.type == KEYDOWN or event.type == MOUSEBUTTONDOWN or event.type == MOUSEBUTTONUP:
-	            controller.handle_pygame_event(event)
-	            
-	        if model.drawTrack == True and model.drawMode == True:
-	            controller.draw_track()
-	        if model.drawTrack == False and model.drawMode ==False and model.offsetMode == True:
-	            controller.offset_track(50)
-	            model.offsetMode = False
-
-	    view.draw3()
-	    time.sleep(0.001)
-
-	pygame.quit()
+screen = pygame.display.set_mode(size)
+model = Model.Platformer_Model(size)
+view = View.PyGameWindowView(model,screen)
+controller = Controller.PyGameController(model)
 
 
+running = True
+while running:
+       
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+        if event.type == KEYDOWN or event.type == MOUSEBUTTONDOWN or event.type == MOUSEBUTTONUP:
+            controller.handle_pygame_event(event)
+            
+        if model.drawTrack == True and model.drawMode == True:
+            controller.draw_track()
+        if model.drawTrack == False and model.drawMode ==False and model.offsetMode == True:
+            controller.offset_track(50)
+            model.offsetMode = False
+      
+    view.draw3()
+    time.sleep(0.001)
 
-	genomeLength = 4
-	gen = driveMatrixEvolution.Genome(genomeLength) 
+pygame.quit()
 
 
-	gen.live() #insert chromosome, test chomosme, repeat
+
+genomeLength = 4
+gen = driveMatrixEvolution.Genome(genomeLength) 
 
 
-	gen.print_genome()
+gen.live() #insert chromosome, test chomosme, repeat
+
+
+gen.print_genome()
 
 
 
