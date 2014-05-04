1a26f6b32ccef7ee35998f793b1ef52ff0201b5e
Josh Sapers <josh@josh-Latitude-E6430.(none)>
2014-05-04 14:15:53 -0400

    fixing graph

--- geneticCar.py
+++ geneticCar.py
@@ -54,7 +54,11 @@ while running:
     if not False in [element.FAIL for element in model.ducks] or model.Killed:
         timeStart = time.time()
         timeElapsed = 0
+<<<<<<< HEAD
         while False:#timeElapsed <5:
+=======
+        while timeElapsed <0:
+>>>>>>> parent of 39c9b5f... before rebase
             print timeElapsed
             view.evolution_animation(timeElapsed)
             timeElapsed = time.time()-timeStart
